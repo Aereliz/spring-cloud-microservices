a9ac9b01f7cb4ea79ae2b36572f2bac3434707b9
Viktor <kliminvictor@gmail.com>
2020-05-11 17:13:37 +0300

    add other projects

--- task-provider/src/test/java/ru/klimin/microservices/taskprovider/TaskProviderApplicationTests.java
+++ task-provider/src/test/java/ru/klimin/microservices/taskprovider/TaskProviderApplicationTests.java
@@ -1,10 +1,10 @@
-package ru.klimin.microservices.taskimpl;
+package ru.klimin.microservices.taskprovider;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskImplApplicationTests {
+class TaskProviderApplicationTests {
 
 	@Test
 	void contextLoads() {
